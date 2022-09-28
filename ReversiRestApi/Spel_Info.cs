82e5586c180f052659f9276541f47c89d4c9313d
damian <damian@profijt.eu>
2022-09-28 22:55:15 +0200

    Update Spel_Info.cs

--- ReversiRestApi/Spel_Info.cs
+++ ReversiRestApi/Spel_Info.cs
@@ -1,6 +1,15 @@
-﻿namespace ReversiRestApi
+﻿using ReversieISpelImplementatie.Model;
+
+namespace ReversiRestApi
 {
     public class Spel_Info
     {
+        public int Id { get; set; }
+        public string SpelToken { get; set; }
+        public string Speler1Token { get; set; }
+        public string Speler2Token { get; set; }
+        public string Omschrijving { get; set; }
+        public Kleur Beurt { get; set; }
+        public string Bord { get; set; }
     }
 }
